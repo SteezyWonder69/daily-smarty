727ac9fef4fc19ac2db9dc6895187dbade3f404b
Spencer Suckow <ssuckow05@gmail.com>
2023-04-14 10:58:37 -0600

    used axios to get recent posts from dailysmartyAPI

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,15 @@
+import axios from "axios"
+
+import { SET_RECENT_POSTS } from "./types"
+
 export function fetchRecentPosts() {
     return function(dispatch) {
-        console.log("hey there");
+        axios.get('https://api.dailysmarty.com/posts')
+        .then(response => {
+            dispatch({
+                type: SET_RECENT_POSTS,
+                payload: response.data.posts
+            })
+        })
     }
 }
